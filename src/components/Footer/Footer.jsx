9de12ffafb19ddd9b68e468d54f812597c2f6094
Mauricio Villarreal <mauricio.villarreal@outlook.com.ar>
2024-09-15 20:03:50 -0300

    Update styles 2003

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,37 @@
+import { Container } from 'react-bootstrap'
 import './Footer.css'
 
 const Footer = () => {
   return (
     <footer>
-        Footer
-        </footer>
+      <Container>
+        
+        
+        <div className="social-media">
+          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+            <img src="./img/facebook-icon.png" alt="Facebook" />
+          </a>
+          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+            <img src="./img/instagram-icon.png" alt="Instagram" />
+          </a>
+          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+            <img src="./img/twitter-icon.png" alt="Twitter" />
+          </a>
+        </div>
+
+        <div className="contact-info">
+          <p>Email: info@lessenza.com</p>
+          <p>Teléfono: +34 123 456 789</p>
+        </div>
+
+       
+
+        <img src="./img/tarjetasfooter.png" alt="Métodos de Pago" />
+        <h6>
+          Copyright L'ESSENZA - 2024. Todos los derechos reservados.
+        </h6>
+      </Container>
+    </footer>
   )
 }
 
--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,29 +1,51 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
+import React, { useContext, useState } from 'react';
+import { Container, Button } from 'react-bootstrap';
+import { CartContext } from '../../CartProvider/CartProvider';
 import "./ItemDetail.css"
 
 export const ItemDetail = ({ product }) => {
+    const { addToCart } = useContext(CartContext);
+    const [quantity, setQuantity] = useState(1);
+
     if (!product) {
         return null;
     }
 
+    // Calcula el valor de cada cuota
+    const installment = (product.price / 3).toFixed(2); // Dividimos el precio por 3 y lo redondeamos a 2 decimales
+
+    const handleAddToCart = () => {
+        addToCart(product._id);
+    };
+
     return (
         <Container>
             <h2>{product.name}</h2>
-          
             <div className='containerDetail'>
                 <div className='imageContainer'>
                     <img src={product.thumbnails} alt={product.name} />
                 </div>
                 <div className='productDetail'>
                     <h4>{product.brands}</h4>
-                    <h6>{product.category}</h6>
-                    <h6>Price: ${product.price}</h6>
+                    <hr />
+                    <h5 className='category'>{product.category}</h5>
+                    <h5 className='code'>Código: {product.code}</h5>
+                    <h5>${product.price}</h5>
+                    <img className='tarjetas' src="./img/tarjetas.png" alt="" />
+                    {/* Cartel de 3 cuotas sin interés */}
+                    <div className="cuotas">
+                    <h6>hasta 3 cuotas de ${installment} sin interés</h6>
+
+                    </div>
+                    <hr />
+                    <button onClick={handleAddToCart}>
+                        COMPRAR
+                    </button>
                 </div>
             </div>
             <hr />
             <h5>Descripción</h5>
-                    <p>{product.description}</p>
+            <p>{product.description}</p>
         </Container>
     );
 };
